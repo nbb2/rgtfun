bc208b3e8f6f8416f557dced0bbcd918b47d68af
Jack <JackG@wirelessprv-10-194-144-158.near.illinois.edu>
2024-09-10 09:54:06 -0500

    added tests for all functions

--- test/TestAnalyticalPotentials.m
+++ test/TestAnalyticalPotentials.m
@@ -18,10 +18,29 @@ classdef TestAnalyticalPotentials < matlab.unittest.TestCase
             expSolution = 2;
             testCase.verifyEqual(actSolution,expSolution,AbsTol=sqrt(eps));
         end
-        function testCoulomb(testCase)
+        function testmy_Coulomb(testCase)
             addpath('../test')
             actSolution = my_coulomb(15,2,0.1:0.1:10); 
             expSolution = load('Coulombpotentialtestdata.mat').testdata;
+            testCase.verifyEqual(actSolution,expSolution,AbsTol=sqrt(eps));
+        end
+        function testmy_difscatter(testCase)
+            addpath('../test')
+            actSolution = my_difscatter(15,2,0:0.1:pi,100); 
+            expSolution = load('Difscattertestdata.mat').ans;
+            testCase.verifyEqual(actSolution,expSolution,AbsTol=sqrt(eps));
+        end
+        function testmy_impact(testCase)
+            addpath('../test')
+            actSolution = my_impact(15,2,0:0.1:pi,100); 
+            expSolution = load('Impactparamtestdata.mat').ans;
+            testCase.verifyEqual(actSolution,expSolution,AbsTol=sqrt(eps));
+        end
+        function testrun_calcpotential(testCase)
+            addpath('../test')
+            run_calcpotential('../test');
+            actSolution = readmatrix("coulomb_data.csv");
+            expSolution = readmatrix("coulomb_testdata.csv");
             testCase.verifyEqual(actSolution,expSolution,AbsTol=sqrt(eps));
         end
     end
